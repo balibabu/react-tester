82698c64f1e8988417448c43b7fa30f8babc6129
balibabu <balibabuchauhan1@gmail.com>
2024-02-28 22:57:18 +0545

    permit

--- src/Test/ClipboardMobile.jsx
+++ src/Test/ClipboardMobile.jsx
@@ -17,12 +17,29 @@ export default function ClipboardMobile() {
             });
     }
 
+    function permit() {
+        navigator.permissions.query({ name: 'clipboard-read' }).then((result) => {
+            if (result.state === 'granted') {
+                setClip('Clipboard access granted.');
+            } else if (result.state === 'prompt') {
+                setClip('Clipboard access prompt.');
+            } else {
+                setClip('Clipboard access denied.');
+            }
+        }).catch((error) => {
+            console.error('Error querying clipboard permissions:', error);
+            setClip('Error querying clipboard permissions.');
+        });
+    }
+    
+
     return (
         <div>
             <div>{clip}</div>
             <div>{errors}</div>
             {!compatible && <input onChange={(e) => setClip(e.target.value)} />}
             <button onClick={pasteClip}>paster</button>
+            <button onClick={permit}>paster</button>
         </div>
     );
 }
