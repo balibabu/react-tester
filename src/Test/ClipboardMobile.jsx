4661c02df8b482ea351f5dc33471936d5c98a854
balibabu <balibabuchauhan1@gmail.com>
2024-02-28 22:47:25 +0545

    clip final

--- src/Test/ClipboardMobile.jsx
+++ src/Test/ClipboardMobile.jsx
@@ -3,26 +3,25 @@ import React, { useState } from 'react';
 export default function ClipboardMobile() {
     const [clip, setClip] = useState('clip-content');
     const [errors, setErrors] = useState('')
+    const [compatible, setCompatible] = useState(true);
 
     function pasteClip() {
-        try {
-            navigator.clipboard.readText()
-                .then(clipboardData => {
-                    setClip(clipboardData);
-                })
-                .catch(error => {
-                    console.error('Failed to read clipboard data: ', error);
-                    setErrors(error.message);
-                });
-        } catch (error) {
-            setClip(error.message);
-        }
+        navigator.clipboard.readText()
+            .then(clipboardData => {
+                setClip(clipboardData);
+            })
+            .catch(error => {
+                console.error('Failed to read clipboard data: ', error);
+                setErrors(error.message);
+                setCompatible(false)
+            });
     }
 
     return (
         <div>
             <div>{clip}</div>
             <div>{errors}</div>
+            {!compatible && <input onChange={(e) => setClip(e.target.value)} />}
             <button onClick={pasteClip}>paster</button>
         </div>
     );
