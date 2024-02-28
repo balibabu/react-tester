badc8203ce79e5f6ce9ef174fd6c82253f803038
balibabu <balibabuchauhan1@gmail.com>
2024-02-28 22:25:50 +0545

    error log

--- src/Test/ClipboardMobile.jsx
+++ src/Test/ClipboardMobile.jsx
@@ -2,21 +2,28 @@ import React, { useState } from 'react';
 
 export default function ClipboardMobile() {
     const [clip, setClip] = useState('clip-content');
+    const [errors, setErrors] = useState('')
 
     function pasteClip() {
-        navigator.clipboard.readText()
-            .then(clipboardData => {
-                setClip(clipboardData);
-            })
-            .catch(error => {
-                console.error('Failed to read clipboard data: ', error);
-            });
+        try {
+            navigator.clipboard.readText()
+                .then(clipboardData => {
+                    setClip(clipboardData);
+                })
+                .catch(error => {
+                    console.error('Failed to read clipboard data: ', error);
+                    setErrors(error);
+                });
+        } catch (error) {
+            setClip(error);
+        }
     }
 
     return (
         <div>
             <div>{clip}</div>
-            <button onClick={pasteClip}>paste</button>
+            <div>{errors}</div>
+            <button onClick={pasteClip}>paster</button>
         </div>
     );
 }
