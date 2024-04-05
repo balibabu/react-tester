7efa1c8a93e83283703ac420c2bb3a95614bed46
balibabu <balibabuchauhan1@gmail.com>
2024-04-05 10:30:40 +0545

    flex-wrap

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export default function Home({ routes }) {
     return (
-        <div className='m-2 d-flex'>
+        <div className='m-2 d-flex flex-wrap'>
             {routes.map((route) => <div className='py-1 px-3 rounded-3 bg-secondary mb-2 mx-2' key={route}><Link to={'/' + route} style={{ textDecoration: 'none', color: 'black', fontSize: '18px', fontWeight: 'bold' }}>{route}</Link></div>)}
         </div>
     )
